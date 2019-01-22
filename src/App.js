95d64b369c5b7b3be9cf0f50e1261f8601f9bb31
miriamfrank23 <miriamfrank23@gmail.com>
2019-01-21 20:53:01 -0500

    fetch working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { connect } from "react-redux";
 
 class App extends Component {
+
+
+  fetchCards = () => {
+    console.log('hi');
+    fetch('http://127.0.0.1:3000/api/cards')
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+  }
+
+  componentDidMount() {
+    this.fetchCards()
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,4 +28,5 @@ class App extends Component {
   }
 }
 
+// export default connect()(App);
 export default App;
