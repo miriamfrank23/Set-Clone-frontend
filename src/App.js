97868495a209beaaba67c16446046464275e41dc
miriamfrank23 <miriamfrank23@gmail.com>
2019-03-27 14:48:24 -0400

    formatting modal

--- src/App.js
+++ src/App.js
@@ -4,24 +4,45 @@ import CardTable from './components/CardTable';
 import GameRules from './components/GameRules';
 import SetList from './components/SetList';
 import SetModal from './components/Modals/SetModal';
+import { connect } from "react-redux";
 
 
-const App = () => {
+
+const App = (props) => {
+
+  const { foundASet, modalShowing } = props
+
+  const renderApp = () => {
+
+  }
 
 
     return (
       <div className='App'>
         <div className='container'>
-          <div className='leftComponents'>
-            <GameRules />
-            <CardTable />
-          </div>
-            <SetList />
-          </div>
-
+        {foundASet && modalShowing ?
+            <SetModal/> :
+            <div>
+              <div className='leftComponents'>
+                <GameRules />
+                <CardTable />
+              </div>
+              <SetList />
+            </div>
+        }
+        </div>
       </div>
     )
 
 }
 
-export default App;
+const mapStateToProps = (state) =>  ({
+  foundASet: state.foundASet,
+  modalShowing: state.modalShowing,
+})
+
+const mapDispatchToProps = (dispatch) =>  ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
